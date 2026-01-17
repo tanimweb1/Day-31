e5634240480d9fe10f37c19c22b6281643fe0adb
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-17 19:07:49 +0600

    Add files via upload

---
+++ Day-31.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+// For loop revise ; 
+
+// how break and continue works in c
+
+for(int i= 1;i<12;i++){
+
+if(i%3==0)
+continue;
+printf("%d\n",i);
+if(i%10==0)
+break;
+
+
+
+}
+
+
+
+
+
+
+
+    return 0;
+}
